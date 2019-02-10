b9774f50dbb8c7f076cb35159f65c57c74c05d52
Valeriy <valor220@gmail.com>
2019-02-10 10:56:43 +0200

    Урок №4-5 Функции. Создание методов объекта

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function start() {
     money = +prompt("Ваш бюджет на месяц?", '');
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-    while(isNaN(money) || money == "" || money == null) {
+    while (isNaN(money) || money == "" || money == null) {
         money = +prompt("Ваш бюджет на месяц?", '');
     }
-} 
+}
 
 start();
 
@@ -17,47 +17,54 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: true
-};
-
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-            b = prompt("Во сколько обойдется?", '');
-        if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' &&
-            a.length < 50) {
-            console.log('done');
-            appData.expenses[a] = b;
-        } else {
-            i = i - 1;
+    savings: true,
+    chooseExpenses: function() {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+                b = prompt("Во сколько обойдется?", '');
+            if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' &&
+                a.length < 50) {
+                console.log('done');
+                appData.expenses[a] = b;
+            } else {
+                i = i - 1;
+            }
         }
-    }
-}
-
-chooseExpenses();
-
-appData.monetPerDay = (appData.budjet / 30).toFixed();
-
-alert("Ужедневный бюджет: " + appData.monetPerDay);
-
-if(appData.monetPerDay < 1000) {
-    console.log("Низкий уровень достатка");
-} else if (appData.monetPerDay > 1000 && appData.monetPerDay < 3000) {
-    console.log("Средний уровень достатка");
-} else if(appData.monetPerDay > 3000) {
-    console.log("Высокий уровень достатка");
-} else {
-    console.log("Произошла ошибка");
-}
-
-function checkSvaings() {
-    if(appData.savings == true) {
-        let save =  +prompt("Какова сумма накоплений?"),
-        percent = +prompt("Под какой процент?");
-
-        appData.monthIncome = save/100/12*percent;
-        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
-    }
-}
-
-checkSvaings();
+    },
+    detectDayBudjet: function() {
+        appData.monetPerDay = (appData.budjet / 30).toFixed();
+        alert("Ежедневный бюджет: " + appData.monetPerDay);
+    },
+    detectLevel: function() {
+        if (appData.monetPerDay < 1000) {
+            console.log("Низкий уровень достатка");
+        } else if (appData.monetPerDay > 1000 && appData.monetPerDay < 3000) {
+            console.log("Средний уровень достатка");
+        } else if (appData.monetPerDay > 3000) {
+            console.log("Высокий уровень достатка");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    checkSvaings: function() {
+        if (appData.savings == true) {
+            let save = +prompt("Какова сумма накоплений?"),
+                percent = +prompt("Под какой процент?");
+    
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+        }
+    },
+    chooseOtpExpenses: function() {
+        for (let i = 0; i < 3; i++) {
+            let opt = prompt("Статья необязательных расходов?", "");
+            appData.optionalExpenses[i] = opt;
+        }
+    },
+    chooseIncome: function() {
+        let items = prompt("Что принесёт дополнительный доход? (Перечислите через запятую)", "");
+        appData.income = items.split(", ");
+        appData.income.push(prompt("Может что-то ещё?", ""));
+        appData.income.sort();
+    }  
+};
